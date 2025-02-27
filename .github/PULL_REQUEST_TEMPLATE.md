095ef548950c2b6d5bec050da4386114e174bc29
poisnada <horberlan@gmail.com>
2025-02-27 19:55:38 -0300

    ci: add mr default template w/ changelog

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,9 @@
+## Description
+
+### Proposed Changes
+- [ ] Feature
+- [ ] Bug fix
+
+### Changelog
+<!-- AUTO-CHANGELOG:START -->
+<!-- AUTO-CHANGELOG:END -->
---
+++ env.sh
@@ -0,0 +1,4 @@
+export PGHOST="ep-delicate-dream-a4r1jztv-pooler.us-east-1.aws.neon.tech"
+export PGDATABASE="neondb"
+export PGUSER="neondb_owner"
+export PGPASSWORD="npg_6nBiV4gdJFLh"
